8e53412501ea19c1222e31294fa6fe02891ed943
Tu Nombre <diegogloria.s@gmail.com>
2024-08-15 14:23:25 +0200

    new file:   datatuning/datatuning.py

---
+++ datatuning/datatuning.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+# Ruta del archivo CSV
+file_path = = os.path.join(script_dir, 'datamunging/consolidated_data.csv')
+df = pd.read_csv(file_path)
+output_path = os.path.join(script_dir, 'datatuning/datatuning.csv')
+df.to_csv(output_path, index=False)
