7bdce090b67b8c75635069d1af9872e20565b1a8
Gloriuss <diegogloria.s@gmail.com>
2024-08-20 17:31:47 +0200

    deleted:    venv/bin/Activate.ps1 	modified:   web_scraping_scripts/web_scraping_criminalidad.py

--- web_scraping_scripts/web_scraping_criminalidad.py
+++ web_scraping_scripts/web_scraping_criminalidad.py
@@ -1,6 +1,7 @@
 import os
+import pandas as pd
 
 def main(script_dir):
     output_path = os.path.join(script_dir, 'data/criminalidad/indice_criminalidad.csv')
-    
+    origin_path = os.path.join(script_dir, 'data/criminalidad/Ind_criminalidad_ESP.csv')
     df.to_csv(output_path, index=False)
