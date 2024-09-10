42731d0869819a8b4c020f030a82f213d1546ac2
Diego Gloria Salamanca <148620227+Gloriuss@users.noreply.github.com>
2024-09-10 12:38:43 +0200

    Add files via upload

--- machinelearning/cross_validation.py
+++ machinelearning/cross_validation.py
@@ -1,161 +1,127 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score, cross_validate, train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.metrics import make_scorer, mean_absolute_error, mean_squared_error, r2_score
+import warnings
 import os
-import joblib
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.pipeline import Pipeline
-import numpy as np
+from sklearn.model_selection import GridSearchCV
 
-def print_statistics(X_test,y_test, y_pred_rf,y_pred_lr, rf_pipeline):
-    ## Evaluación con filtrado de variables
-    # Filtrar filas donde 'latitude' o 'longitude' no sean NaN en el conjunto de prueba
-    valid_indices = np.where(~X_test[['latitude', 'longitude']].isna().any(axis=1))[0]
-    y_test_filtered = y_test.iloc[valid_indices]
-    y_pred_rf_filtered = y_pred_rf[valid_indices]
-    y_pred_lr_filtered = y_pred_lr[valid_indices]
+warnings.filterwarnings('ignore')
 
-    # Métricas para LinearRegression
-    mae_lr_full = mean_absolute_error(y_test, y_pred_lr)
-    mse_lr_full = mean_squared_error(y_test, y_pred_lr)
-    rmse_lr_full = np.sqrt(mse_lr_full)
-    r2_lr_full = r2_score(y_test, y_pred_lr)
-    print("\nResultados de LinearRegression (sin filtrar):")
-    print(f'MAE: {mae_lr_full}')
-    print(f'RMSE: {rmse_lr_full}')
-    print(f'R2 Score: {r2_lr_full}')
+# Función de preprocesamiento general
+def divide_dataset_bycategory(df, dummies): 
+    dummy_dfs = []
+    if dummies:
+        for dummy_col in dummies:
+            unique_values = df[dummy_col].unique()
+            for val in unique_values:
+                df_subset = df[df[dummy_col] == val].copy()
+                dummy_dfs.append((dummy_col, val, df_subset))
 
-    # Métricas para LinearRegression usando solo las filas filtradas
-    mae_lr_filtered = mean_absolute_error(y_test_filtered, y_pred_lr_filtered)
-    mse_lr_filtered = mean_squared_error(y_test_filtered, y_pred_lr_filtered)
-    rmse_lr_filtered = np.sqrt(mse_lr_filtered)
-    r2_lr_filtered = r2_score(y_test_filtered, y_pred_lr_filtered)
-    print("\nResultados de LinearRegression (filtrados lat y lon no son Nan):")
-    print(f'MAE: {mae_lr_filtered}')
-    print(f'RMSE: {rmse_lr_filtered}')
-    print(f'R2 Score: {r2_lr_filtered}')
+    return dummy_dfs if dummy_dfs else [(None, None, df)]
 
-    ## Evaluación sin filtrar variables (resultados completos)
-    # Métricas para RandomForest
-    mae_rf_full = mean_absolute_error(y_test, y_pred_rf)
-    mse_rf_full = mean_squared_error(y_test, y_pred_rf)
-    rmse_rf_full = np.sqrt(mse_rf_full)
-    r2_rf_full = r2_score(y_test, y_pred_rf)
-    print("Resultados de RandomForest (sin filtrar):")
-    print(f'MAE: {mae_rf_full}')
-    print(f'RMSE: {rmse_rf_full}')
-    print(f'R2 Score: {r2_rf_full}')
+# Función para preprocesar las variables independientes
+def process_features(df, target):
+    label_encoders = {}
+    columns_to_drop = []
+    categorical_columns = df.select_dtypes(include=['object']).columns
 
-    # Métricas para RandomForest usando solo las filas filtradas
-    mae_rf_filtered = mean_absolute_error(y_test_filtered, y_pred_rf_filtered)
-    mse_rf_filtered = mean_squared_error(y_test_filtered, y_pred_rf_filtered)
-    rmse_rf_filtered = np.sqrt(mse_rf_filtered)
-    r2_rf_filtered = r2_score(y_test_filtered, y_pred_rf_filtered)
-    print("\nResultados de RandomForest (filtrados lat y lon no son Nan):")
-    print(f'MAE: {mae_rf_filtered}')
-    print(f'RMSE: {rmse_rf_filtered}')
-    print(f'R2 Score: {r2_rf_filtered}')
+    # Procesar columnas de tipo 'object'
+    for col in categorical_columns:
+        df[col] = df[col].astype(str)  # Asegúrate de que todos los valores sean cadenas de texto
+        unique_vals = df[col].nunique()
+        if unique_vals > 20:
+            columns_to_drop.append(col)
+        else:
+            df[col] = df[col].fillna('0')
+            le = LabelEncoder()
+            df[col] = le.fit_transform(df[col])
+            label_encoders[col] = le
 
-    # Imprimir algunas predicciones filtradas para verificar
-    print("\nAlgunas predicciones de RandomForest (filtrados):")
-    for i in range(min(5, len(valid_indices))):
-        print(f"Precio real: {y_test_filtered.iloc[i]}, Precio predicho: {y_pred_rf_filtered[i]}")
-    print("\nAlgunas predicciones de LinearRegression (filtrados):")
-    for i in range(min(5, len(valid_indices))):
-        print(f"Precio real: {y_test_filtered.iloc[i]}, Precio predicho: {y_pred_lr_filtered[i]}")
+    # Procesar columnas numéricas
+    numerical_columns = df.select_dtypes(include=[np.number]).columns
+    numerical_columns = numerical_columns.drop(target)
 
-    # Obtener importancia de características de RandomForest
-    rf_model = rf_pipeline.named_steps['regressor']
-    importances = rf_model.feature_importances_
-    feature_names = rf_pipeline.named_steps['preprocessor'].get_feature_names_out()
+    scaler = StandardScaler()
+    df[numerical_columns] = scaler.fit_transform(df[numerical_columns])
 
-    # Crear un DataFrame con la importancia de características
-    importance_df = pd.DataFrame({'feature': feature_names, 'importance': importances})
-    importance_df = importance_df.sort_values('importance', ascending=False)
-    print("\nCaracterísticas más importantes de RandomForest:")
-    print(importance_df.head(10))
+    # Eliminar columnas con un solo valor
+    for col in df.columns:
+        if df[col].nunique() == 1:
+            columns_to_drop.append(col)
 
+    df = df.drop(columns=columns_to_drop)
+    return df, scaler, label_encoders
 
-def machine_learning(script_dir):
-    # Ruta del archivo CSV
-    file_path = os.path.join(script_dir, 'datamunging/consolidated_data_DT.csv')
+# Función para entrenar el primer modelo
+def train_first_model(df, target):
+    X = df.drop(columns=[target])
+    y = df[target]
 
-    # Leer el archivo CSV en un DataFrame
-    df = pd.read_csv(file_path)
-    # Verificar el tamaño del DataFrame
-    print(f"Tamaño del DataFrame: {df.shape}")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Eliminar filas donde 'precio' es NaN, 0, o infinito
-    df = df[~df['precio'].isin([0, np.inf, -np.inf]) & df['precio'].notna()]
-    df = df[~df['mt2'].isin([0, np.inf, -np.inf]) & df['mt2'].notna()]
-    #df = df.drop(['planta', 'publicado_hace'], axis=1)
-    
-    # Separar características y variable objetivo
-    X = df.drop(['precio'], axis=1)
-    y = df['precio']
+    model = RandomForestRegressor(n_estimators=100, random_state=42, n_jobs=-1)
+    model.fit(X_train, y_train)
 
-    # Identificar columnas numéricas
-    num_cols = X.select_dtypes(include=['number']).columns
+    y_pred = model.predict(X_test)
+    mae = mean_absolute_error(y_test, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    r2 = r2_score(y_test, y_pred)
 
-    # Preprocesamiento para columnas numéricas
-    num_transformer = Pipeline(steps=[
-    #    ('imputer', SimpleImputer(strategy='mean')),
-        ('scaler', StandardScaler())
-    ])
+    feature_importances = pd.Series(model.feature_importances_, index=X.columns)
+    top_50_features = feature_importances.nlargest(50).index
 
-    # Combinar preprocesadores solo para columnas numéricas
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('', num_transformer, num_cols)
-        ])
-    # Dividir el conjunto de datos en entrenamiento y prueba
-    # Definir los hiperparámetros y sus rangos
+    return model, top_50_features
+
+def grid_search_cv_model(df, target, top_features, cv=5):
+    X = df[top_features]
+    y = df[target]
+
+    model = RandomForestRegressor(random_state=42, n_jobs=-1)
+
+    # Definir los parámetros para la búsqueda de hiperparámetros
     param_dist = {
-        'regressor__n_estimators': [100, 150, 200, 500],
-        'regressor__max_depth': [20, 30, 50, None],
-        'regressor__min_samples_split': [2, 5, 10],
-        'regressor__max_features': ['auto', 'sqrt', 'log2', 0.2, 0.5, 1.0],
-        'regressor__bootstrap': [True, False],
-        'regressor__criterion': ['squared_error']
+        'n_estimators': [100, 150, 200, 500],
+        'max_depth': [20, 30, 50, None],
+        'min_samples_split': [2, 5, 10],
+        'max_features': ['auto', 'sqrt', 'log2', 0.2, 0.5, 1.0],
+        'bootstrap': [True, False],
+        'criterion': ['squared_error']
     }
 
-    # Crear el pipeline como antes
-    rf_pipeline = Pipeline([
-        ('preprocessor', preprocessor),
-        ('regressor', RandomForestRegressor(random_state=42))
-    ])
+    grid_search = GridSearchCV(estimator=model, param_grid=param_dist, 
+                               cv=cv, scoring='r2', n_jobs=-1, verbose=3)
+    
+    grid_search.fit(X, y)
+    
+    # Imprimir los mejores parámetros y el mejor score obtenido
+    print(f"Mejores parámetros encontrados: {grid_search.best_params_}")
+    print(f"Mejor R2 obtenido: {grid_search.best_score_}")
+    
+    return grid_search.best_estimator_
 
-    # Configurar el RandomizedSearchCV
-    random_search = RandomizedSearchCV(
-        rf_pipeline, 
-        param_distributions=param_dist, 
-        n_iter=50,  # Número de combinaciones aleatorias a probar
-        cv=5,  # Número de folds para la validación cruzada
-        verbose=3,  # Nivel de detalle de la salida
-        n_jobs=-1,  # Usar todos los núcleos disponibles
-        random_state=42
-    )
-    X_train, X_test, y_train, y_test = train_test_split(X[num_cols], y, test_size=0.2, random_state=42)
-    # Entrenar con RandomizedSearchCV
-    random_search.fit(X_train, y_train)
+# Función principal para ejecutar el proceso completo
+def main(target='precio', dummies=['alquiler_venta']):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    script_dir = os.path.join(script_dir, '../datamunging/consolidated_data.csv')
+    df = pd.read_csv(script_dir)
+    print(f"Tamaño del DataFrame inicial: {df.shape}")
+    
+    dummy_dfs = divide_dataset_bycategory(df, dummies)
 
-    # Mejor combinación de hiperparámetros
-    print(f"Mejores hiperparámetros encontrados: {random_search.best_params_}")
+    for dummy_info, val, df_dummy in dummy_dfs:
+        print(f"Entrenando modelos para {dummy_info}={val}..." if dummy_info else "Entrenando modelo general...")
 
+        # Preprocesamiento de las características
+        df_processed, scaler, label_encoders = process_features(df_dummy, target)
 
+        # Entrenar el primer modelo
+        _, top_50_features = train_first_model(df_processed, target)
 
-def main():
-    # Carpeta principal (path relativo en la ubicación local del proyecto)
-    script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    machine_learning(script_dir)
+        # Realizar la búsqueda de hiperparámetros con las 50 mejores características
+        best_model = grid_search_cv_model(df_processed, target, top_50_features, cv=5)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
