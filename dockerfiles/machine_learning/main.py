b734fc353c5262792b6a95c94eb8e7b50f2b751a
SrOlalau <144601654+SrOlalau@users.noreply.github.com>
2024-09-12 14:03:00 +0200

    Update main.py

--- dockerfiles/machine_learning/main.py
+++ dockerfiles/machine_learning/main.py
@@ -1,20 +1,20 @@
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 import optuna
+from optuna.integration import OptunaSearchCV
 import warnings
 import time
 from datetime import datetime
-from sqlalchemy import create_engine, text
-
-
+import pickle
+import os
 
 warnings.filterwarnings('ignore')
 
@@ -33,28 +33,20 @@ def load_data_from_postgres():
     connection_string = f"postgresql://{DB_DEST['USER']}:{DB_DEST['PASSWORD']}@{DB_DEST['HOST']}:{DB_DEST['PORT']}/{DB_DEST['NAME']}"
     engine = create_engine(connection_string)
     
-    # Identificar columnas de fecha
     with engine.connect() as connection:
         query = text(f"SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '{DB_DEST['TABLE']}' AND data_type LIKE '%timestamp%'")
         result = connection.execute(query)
         date_columns = [row[0] for row in result]
 
-    # Cargar datos con parse_dates
     query = f'SELECT * FROM "{DB_DEST["TABLE"]}"'
     df = pd.read_sql(query, engine, parse_dates=date_columns)
     print(f"Tamaño del DataFrame cargado: {df.shape}")
     return df
 
-def divide_dataset_bycategory(df, dummies):
+def divide_dataset_bycategory(df, category):
     """Divide el dataset por categorías."""
-    dummy_dfs = []
-    if dummies:
-        for dummy_col in dummies:
-            unique_values = df[dummy_col].unique()
-            for val in unique_values:
-                df_subset = df[df[dummy_col] == val].copy()
-                dummy_dfs.append((dummy_col, val, df_subset))
-    return dummy_dfs if dummy_dfs else [(None, None, df)]
+    df_subset = df[df['alquiler_venta'] == category].copy()
+    return df_subset
 
 def process_features(df, target):
     """Preprocesa las características del DataFrame."""
@@ -64,7 +56,6 @@ def process_features(df, target):
     numerical_columns = df.select_dtypes(include=[np.number]).columns.drop(target)
     datetime_columns = df.select_dtypes(include=['datetime64']).columns
 
-    # Procesar columnas categóricas
     for col in categorical_columns:
         df[col] = df[col].astype(str)
         unique_vals = df[col].nunique()
@@ -76,18 +67,15 @@ def process_features(df, target):
             df[col] = le.fit_transform(df[col])
             label_encoders[col] = le
 
-    # Procesar columnas numéricas
     scaler = StandardScaler()
     df[numerical_columns] = scaler.fit_transform(df[numerical_columns])
 
-    # Procesar columnas de fecha
     for col in datetime_columns:
         df[f'{col}_year'] = df[col].dt.year
         df[f'{col}_month'] = df[col].dt.month
         df[f'{col}_day'] = df[col].dt.day
         columns_to_drop.append(col)
 
-    # Eliminar columnas con un solo valor
     for col in df.columns:
         if df[col].nunique() == 1:
             columns_to_drop.append(col)
@@ -135,32 +123,34 @@ def train_first_model(df, target):
 
     return model, top_50_features, X_train, X_test, y_train, y_test
 
-def objective(trial, X, y):
-    """Función objetivo para Optuna."""
-    params = {
-        'n_estimators': trial.suggest_int('n_estimators', 100, 1000),
-        'max_depth': trial.suggest_int('max_depth', 10, 100),
-        'min_samples_split': trial.suggest_int('min_samples_split', 2, 10),
-        'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 10),
-        'max_features': trial.suggest_categorical('max_features', ['sqrt', 'log2', None]),
-        'bootstrap': trial.suggest_categorical('bootstrap', [True, False])
-    }
-    
-    model = RandomForestRegressor(**params, random_state=42, n_jobs=-1)
-    return -np.mean(cross_val_score(model, X, y, cv=5, scoring='neg_mean_squared_error'))
-
 def optimize_hyperparameters(X, y):
-    """Optimiza los hiperparámetros usando Optuna."""
-    study = optuna.create_study(direction='minimize')
-    study.optimize(lambda trial: objective(trial, X, y), n_trials=100)
+    """Optimiza los hiperparámetros usando OptunaSearchCV."""
+    param_distributions = {
+        'n_estimators': optuna.distributions.IntDistribution(100, 1000),
+        'max_depth': optuna.distributions.IntDistribution(10, 100),
+        'min_samples_split': optuna.distributions.IntDistribution(2, 10),
+        'min_samples_leaf': optuna.distributions.IntDistribution(1, 10),
+        'max_features': optuna.distributions.CategoricalDistribution(['sqrt', 'log2', None]),
+        'bootstrap': optuna.distributions.CategoricalDistribution([True, False])
+    }
+
+    optuna_search = OptunaSearchCV(
+        RandomForestRegressor(random_state=42, n_jobs=-1),
+        param_distributions,
+        n_trials=75,
+        cv=5,
+        n_jobs=-1,
+        random_state=42
+    )
+
+    optuna_search.fit(X, y)
     
     print("Mejores hiperparámetros encontrados:")
-    print(study.best_params)
-    return study.best_params
+    print(optuna_search.best_params_)
+    return optuna_search.best_estimator_
 
-def train_and_evaluate_model(X_train, X_test, y_train, y_test, best_params):
-    """Entrena el modelo con los mejores hiperparámetros y evalúa su rendimiento."""
-    model = RandomForestRegressor(** best_params, random_state=42, n_jobs=-1)
+def train_and_evaluate_model(X_train, X_test, y_train, y_test, model):
+    """Evalúa el rendimiento del modelo."""
     model.fit(X_train, y_train)
     
     y_pred = model.predict(X_test)
@@ -186,42 +176,42 @@ def train_and_evaluate_model(X_train, X_test, y_train, y_test, best_params):
 
     return model, feature_importances
 
-def main(target='precio', dummies=['alquiler_venta']):
+def main(target='precio', category='venta'):
     start_time = time.time()
     print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Proceso iniciado...")
 
-    # Cargar datos
     df = load_data_from_postgres()
     
-    # Dividir el dataset por categorías
-    dummy_dfs = divide_dataset_bycategory(df, dummies)
+    df_category = divide_dataset_bycategory(df, category)
 
-    for dummy_info, val, df_dummy in dummy_dfs:
-        print(f"\nEntrenando modelos para {dummy_info}={val}..." if dummy_info else "\nEntrenando modelo general...")
+    print(f"\nEntrenando modelo para {category}...")
 
-        # Preprocesar características
-        df_processed, _, _ = process_features(df_dummy, target)
+    df_processed, _, _ = process_features(df_category, target)
 
-        # Entrenar el primer modelo y seleccionar las mejores características
-        _, top_50_features, X_train, X_test, y_train, y_test = train_first_model(df_processed, target)
+    _, top_50_features, X_train, X_test, y_train, y_test = train_first_model(df_processed, target)
 
-        # Usar solo las 50 mejores características
-        X_train_top = X_train[top_50_features]
-        X_test_top = X_test[top_50_features]
+    X_train_top = X_train[top_50_features]
+    X_test_top = X_test[top_50_features]
 
-        # Optimizar hiperparámetros
-        best_params = optimize_hyperparameters(X_train_top, y_train)
+    best_model = optimize_hyperparameters(X_train_top, y_train)
 
-        # Entrenar y evaluar el modelo final
-        final_model, feature_importances = train_and_evaluate_model(X_train_top, X_test_top, y_train, y_test, best_params)
+    final_model, feature_importances = train_and_evaluate_model(X_train_top, X_test_top, y_train, y_test, best_model)
 
-        print("\nResumen del modelo:")
-        print(f"Número de características utilizadas: {len(top_50_features)}")
-        print(f"Tamaño del conjunto de entrenamiento: {X_train_top.shape}")
-        print(f"Tamaño del conjunto de prueba: {X_test_top.shape}")
-        print("\nMejores hiperparámetros:")
-        for param, value in best_params.items():
-            print(f"{param}: {value}")
+    print("\nResumen del modelo:")
+    print(f"Número de características utilizadas: {len(top_50_features)}")
+    print(f"Tamaño del conjunto de entrenamiento: {X_train_top.shape}")
+    print(f"Tamaño del conjunto de prueba: {X_test_top.shape}")
+    print("\nMejores hiperparámetros:")
+    for param, value in best_model.get_params().items():
+        print(f"{param}: {value}")
+
+    # Exportar el modelo
+    output_dir = '/resultado'
+    os.makedirs(output_dir, exist_ok=True)
+    model_filename = f'modelo_{category}.pickle'
+    with open(os.path.join(output_dir, model_filename), 'wb') as f:
+        pickle.dump(final_model, f)
+    print(f"\nModelo guardado en: {os.path.join(output_dir, model_filename)}")
 
     end_time = time.time()
     elapsed_time = end_time - start_time
@@ -230,5 +220,5 @@ def main(target='precio', dummies=['alquiler_venta']):
     print(f"\nTiempo total de ejecución: {int(hours)}h {int(minutes)}m {int(seconds)}s")
 
 if __name__ == '__main__':
-    main()
-
+    # Cambia 'venta' por 'alquiler' si quieres entrenar el modelo para alquiler
+    main(category='venta')
