7c2d0cf39eba34172f021e7f12522c911565b8bf
SrOlalau <144601654+SrOlalau@users.noreply.github.com>
2025-05-07 22:33:01 +0200

    Update main.py

--- dockerfiles/4.Data_tunning/main.py
+++ dockerfiles/4.Data_tunning/main.py
@@ -24,8 +24,8 @@ DB_DEST = {
 }
 
 # Configuración de Telegram
-TELEGRAM_BOT_TOKEN = '6916058231:AAEOmgGX0k427p5mbe6UFmxAL1MpTXYCYTs'
-TELEGRAM_CHAT_ID = '297175679'
+TELEGRAM_BOT_TOKEN = ':'
+TELEGRAM_CHAT_ID = ''
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
